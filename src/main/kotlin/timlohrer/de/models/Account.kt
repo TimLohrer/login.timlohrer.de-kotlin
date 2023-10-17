223c62e42dd2e0d25ca31be5b981042b9ab7b66c
Tim Lohrer <me@timlohrer.de>
2023-10-17 22:48:51 +0200

    added default values to account model

--- src/main/kotlin/timlohrer/de/models/Account.kt
+++ src/main/kotlin/timlohrer/de/models/Account.kt
@@ -1,15 +1,15 @@
 package timlohrer.de.models
 
 data class Account (
-    var _id: String,
-    var firstName: String,
-    var lastName: String,
-    var email: String,
-    var password: String,
-    var twoFactorAuth: Boolean,
-    var twoFactorAuthKey: String,
-    var createdAt: String,
-    var lastLogin: String,
-    var roles: List<String>,
-    var disabled: Boolean
+    var _id: String = "",
+    var firstName: String = "",
+    var lastName: String = "",
+    var email: String = "",
+    var password: String = "",
+    var twoFactorAuth: Boolean = false,
+    var twoFactorAuthKey: String = "",
+    var createdAt: Long = 0,
+    var lastLogin: Long = 0,
+    var roles: List<String> = listOf(),
+    var disabled: Boolean = false
 )
