b1d5fdb9215914a30c47f338a3e60fe5535cb20b
Tim Lohrer <me@timlohrer.de>
2023-10-18 01:13:52 +0200

    switched to totp package

--- src/main/kotlin/timlohrer/de/utils/TwoFactorAuthManager.kt
+++ src/main/kotlin/timlohrer/de/utils/TwoFactorAuthManager.kt
@@ -4,23 +4,21 @@ import com.google.zxing.BarcodeFormat
 import com.google.zxing.MultiFormatWriter
 import com.google.zxing.client.j2se.MatrixToImageWriter
 import com.google.zxing.common.BitMatrix
+import dev.turingcomplete.kotlinonetimepassword.GoogleAuthenticator
+import org.apache.commons.codec.binary.Base32
 import java.io.ByteArrayOutputStream
-import java.nio.ByteBuffer
-import java.security.Key
 import java.security.SecureRandom
 import java.util.*
-import javax.crypto.Mac
-import javax.crypto.spec.SecretKeySpec
-import kotlin.experimental.and
 
 class TwoFactorAuthManager {
     val timeStep: Int = 30;
     val codeLength: Int = 6;
+
     fun generateSecretKey(): String {
         val random = SecureRandom();
-        val secretKey = ByteArray(20);
+        val secretKey = ByteArray(48);
         random.nextBytes(secretKey);
-        return secretKey.toString();
+        return Base32(false).encodeAsString(secretKey).uppercase();
     }
 
     fun generateQrCode(email: String, secretKey: String): String {
@@ -39,39 +37,7 @@ class TwoFactorAuthManager {
         return "data:image/png;base64,$base64Image";
     }
 
-    fun generateTOTP(secretKey: String): String {
-        val time = System.currentTimeMillis() / 1000 / timeStep;
-        return generateTOTP(secretKey.toByteArray(), time);
-    }
-
     fun validateTOTP(secretKey: String, userProvidedCode: String): Boolean {
-        val currentTime = System.currentTimeMillis() / 1000 / timeStep;
-
-        for (i in -1..1) {
-            val candidateTime = currentTime + i;
-            val candidateCode = generateTOTP(secretKey.toByteArray(), candidateTime);
-            if (candidateCode == userProvidedCode) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private fun generateTOTP(key: ByteArray, time: Long): String {
-        val msg = ByteBuffer.allocate(8).putLong(time).array()
-        val hmac = getHMAC(key, msg)
-        val offset = hmac[hmac.size - 1] and 0xF
-
-        val truncatedHash = hmac.copyOfRange(offset.toInt(), offset + 4)
-        val code = ByteBuffer.wrap(truncatedHash).int and 0x7FFFFFFF % 1000000
-
-        return code.toString().padStart(codeLength, '0')
-    }
-
-    private fun getHMAC(key: ByteArray, data: ByteArray): ByteArray {
-        val signingKey: Key = SecretKeySpec(key, "HmacSHA1");
-        val mac = Mac.getInstance("HmacSHA1");
-        mac.init(signingKey);
-        return mac.doFinal(data);
+        return GoogleAuthenticator(secretKey).isValid(userProvidedCode);
     }
 }
