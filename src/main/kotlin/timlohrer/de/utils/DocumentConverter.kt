814ffc9073c6a96b27a31efbbc70707c80f9bb42
Tim Lohrer <me@timlohrer.de>
2023-10-17 22:48:04 +0200

    updated document converter

--- src/main/kotlin/timlohrer/de/utils/DocumentConverter.kt
+++ src/main/kotlin/timlohrer/de/utils/DocumentConverter.kt
@@ -1,6 +1,7 @@
 package timlohrer.de.utils
 
 import org.bson.Document
+import java.lang.reflect.Modifier
 
 inline fun <reified T : Any> T.toDocument(): Document {
     val document = Document();
@@ -9,4 +10,30 @@ inline fun <reified T : Any> T.toDocument(): Document {
         document.append(property.name, property.get(this));
     }
     return document;
+}
+
+inline fun <reified T : Any> Document.toDataClass(
+    fieldMappings: Map<String, String> = emptyMap(),
+    excludeFields: Set<String> = emptySet()
+): T {
+    val clazz = T::class.java
+    val instance = clazz.getDeclaredConstructor().newInstance()
+
+    for (property in clazz.declaredFields) {
+        if (!Modifier.isStatic(property.modifiers)) {
+            property.isAccessible = true
+            val fieldName = fieldMappings[property.name] ?: property.name
+
+            if (!excludeFields.contains(fieldName)) {
+                val rawValue = this[fieldName]
+                val value = when (property.type) {
+                    String::class.java -> rawValue?.toString() ?: ""
+                    else -> rawValue
+                }
+                property.set(instance, value)
+            }
+        }
+    }
+
+    return instance
 }
