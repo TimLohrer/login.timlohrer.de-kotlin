d9e076ccbaf0b0e76001ee67b82f73ba649fe779
Tim Lohrer <me@timlohrer.de>
2023-10-18 23:26:18 +0200

    removed test db and jwt key from config

--- src/main/kotlin/timlohrer/de/config/Config.kt
+++ src/main/kotlin/timlohrer/de/config/Config.kt
@@ -2,8 +2,8 @@ package timlohrer.de.config
 
 class Config {
     val port: Int = 80;
-    val mongoUri: String = "mongodb://192.168.178.99:4002";
-    val JWTSecret: String = "h%tW@fRe2@z#TEp9ATdBus#zRa79h!^3#2E24hY%";
+    val mongoUri: String = "";  // insert mongo uri here
+    val JWTSecret: String = ""; // insert private jwt secret here
     val defaultTokenLifespand: Long = 1;
     val rememberMeTokenLifespand: Long = 30;
 }
