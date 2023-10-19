4e04c2d18f9cc56d00e313d2d5a40fdb1c76a8e2
Tim Lohrer <me@timlohrer.de>
2023-10-19 14:31:45 +0200

    change default port to 8080

--- src/main/kotlin/timlohrer/de/config/Config.kt
+++ src/main/kotlin/timlohrer/de/config/Config.kt
@@ -1,7 +1,7 @@
 package timlohrer.de.config
 
 class Config {
-    val port: Int = 80;
+    val port: Int = 8080;
     val mongoUri: String = "";  // insert mongo uri here
     val JWTSecret: String = ""; // insert private jwt secret here
     val defaultTokenLifespand: Long = 1;
